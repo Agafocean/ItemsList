981d49532b2a0fabe9c0b9d880dc16136d3bcd10
Agafocean <158978662+Agafocean@users.noreply.github.com>
2024-03-17 15:47:31 +1000

    Update indexTemplate.js

--- src/server/indexTemplate.js
+++ src/server/indexTemplate.js
@@ -2,8 +2,7 @@ export const indexTemplate = (content) => `
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">    
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">       
     <title>Reddit</title>    
